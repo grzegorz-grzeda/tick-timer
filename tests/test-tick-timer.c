547991802d023fb341dafecb2483c5b9a66e6c8a
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2023-10-14 15:48:07 +0200

    Added basic tests

--- tests/test-tick-timer.c
+++ tests/test-tick-timer.c
@@ -28,12 +28,34 @@
 #include "cmocka.h"
 
 #include "tick-timer.h"
+void* __real_calloc(size_t number_of_elements, size_t element_size);
+void __real_free(void* ptr);
 
-static void test_null(void** state) {}
+void* __wrap_calloc(size_t number_of_elements, size_t element_size) {
+    return __real_calloc(number_of_elements, element_size);
+}
+
+void __wrap_free(void* ptr) {
+    __real_free(ptr);
+}
+
+static void test_dummy(void** state) {}
+
+static void test_invalid_create_tick_timer(void** state) {
+    tick_timer_t* timer = tick_timer_create(0);
+    assert_ptr_equal(timer, NULL);
+}
+
+static void test_create_destroy_tick_timer(void** state) {
+    tick_timer_t* timer = tick_timer_create(10);
+    assert_ptr_not_equal(timer, NULL);
+    tick_timer_destroy(timer);
+}
 
 int main(int argc, char** argv) {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_null),
+        cmocka_unit_test(test_invalid_create_tick_timer),
+        cmocka_unit_test(test_create_destroy_tick_timer),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
